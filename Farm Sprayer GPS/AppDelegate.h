504c536edef27096fbc4d54d3098ab7c9e964a5d
Joshua Johnson <johnsonjp34@joshuas-mbp.hh.nku.edu>
2015-08-18 15:08:41 -0400

    Initial Commit

---
+++ Farm Sprayer GPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farm Sprayer GPS
+//
+//  Created by Joshua Johnson on 8/18/15.
+//  Copyright (c) 2015 Investor Direction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farm Sprayer GPS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farm Sprayer GPS
+//
+//  Created by Joshua Johnson on 8/18/15.
+//  Copyright (c) 2015 Investor Direction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
